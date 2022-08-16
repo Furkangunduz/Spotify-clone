7ecc1113ef395a800948ff3be7080400f9811152
Furkan <furkan.gundz00@gmail.com>
2022-08-17 01:20:58 +0300

    Created Search page.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,14 @@
 import Navigation from 'components/Navbar/Navigation';
 import Auth from 'components/Navbar/Auth';
 import { useLocation } from 'react-router-dom';
+import Search from 'components/Navbar/Search';
 
 function Navbar() {
 	const { pathname } = useLocation();
 	return (
 		<nav className='h-[3.75rem] w-full flex items-center justify-between px-8'>
 			<Navigation />
-			{pathname == '/search' && 'Search'}
+			{pathname == '/search' && <Search />}
 			<Auth />
 		</nav>
 	);
